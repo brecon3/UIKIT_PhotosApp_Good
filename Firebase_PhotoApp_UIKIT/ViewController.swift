38a96965c12c47cde127cc410832e0cbbd3d2f3b
brecon3 <beaconbrecon@tutamail.com>
2021-06-23 15:37:42 -0400

    Initial Commit

---
+++ Firebase_PhotoApp_UIKIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase_PhotoApp_UIKIT
+//
+//  Created by Spadafora on 6/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
